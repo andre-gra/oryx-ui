635fbcfdc27249528d89cd877565252bad2ffd98
andregra <andregra717@gmail.com>
2023-07-12 16:56:27 +0200

    move classes inside components

--- src/ui/demo/SelectDemo.tsx
+++ src/ui/demo/SelectDemo.tsx
@@ -3,37 +3,40 @@ import * as Select from '../components/Select'
 import { SelectGroupItem } from '../components/Select/SelectGroupItem'
 
 const SelectDemo = () => (
-  <Select.Root>
+  <Select.Root
+    onOpenChange={() =>window.alert('openOnChange')}
+    defaultOpen={true}
+    >
     <Select.Trigger
-      className="inline-flex items-center justify-center rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none"
       aria-label="Food"
+      backgroundColor="bg-red6"
     >
       <Select.Value placeholder="Select a fruit…" />
-      <Select.Icon className="text-violet11">
+      <Select.Icon>
         <ChevronDownIcon />
       </Select.Icon>
     </Select.Trigger>
     <Select.Portal>
-      <Select.Content className="overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
-        <Select.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
+      <Select.Content>
+        <Select.ScrollUpButton>
           <ChevronUpIcon />
         </Select.ScrollUpButton>
-        <Select.Viewport className="p-[5px]">
+        <Select.Viewport>
           <Select.Group>
-            <Select.Label className="px-[25px] text-xs leading-[25px] text-mauve11">
+            <Select.Label>
               Fruits
             </Select.Label>
-            <SelectGroupItem value="apple">Apple</SelectGroupItem>
+            <SelectGroupItem className='text-red-600' value="apple">Apple</SelectGroupItem>
             <SelectGroupItem value="banana">Banana</SelectGroupItem>
             <SelectGroupItem value="blueberry">Blueberry</SelectGroupItem>
             <SelectGroupItem value="grapes">Grapes</SelectGroupItem>
             <SelectGroupItem value="pineapple">Pineapple</SelectGroupItem>
           </Select.Group>
 
-          <Select.Separator className="h-[1px] bg-violet6 m-[5px]" />
+          <Select.Separator />
 
           <Select.Group>
-            <Select.Label className="px-[25px] text-xs leading-[25px] text-mauve11">
+            <Select.Label>
               Vegetables
             </Select.Label>
             <SelectGroupItem value="aubergine">Aubergine</SelectGroupItem>
@@ -45,10 +48,10 @@ const SelectDemo = () => (
             <SelectGroupItem value="leek">Leek</SelectGroupItem>
           </Select.Group>
 
-          <Select.Separator className="h-[1px] bg-violet6 m-[5px]" />
+          <Select.Separator />
 
           <Select.Group>
-            <Select.Label className="px-[25px] text-xs leading-[25px] text-mauve11">
+            <Select.Label>
               Meat
             </Select.Label>
             <SelectGroupItem value="beef">Beef</SelectGroupItem>
@@ -57,7 +60,7 @@ const SelectDemo = () => (
             <SelectGroupItem value="pork">Pork</SelectGroupItem>
           </Select.Group>
         </Select.Viewport>
-        <Select.ScrollDownButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
+        <Select.ScrollDownButton>
           <ChevronDownIcon />
         </Select.ScrollDownButton>
       </Select.Content>
