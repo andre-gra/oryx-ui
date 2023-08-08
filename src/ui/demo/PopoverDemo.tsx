57f1eda52ffab2db270016757d04a18ac17f9819
andregra <andregra717@gmail.com>
2023-08-08 09:06:07 +0200

    set rounded by size

--- src/ui/demo/PopoverDemo.tsx
+++ src/ui/demo/PopoverDemo.tsx
@@ -40,7 +40,7 @@ const PopoverDemo = ({ ...props }: PopoverProps) => {
           className={classnames(
             theme,
             `popover-content${size}`,
-            "rounded p-5 bg-color3 shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_hsla(206,22%,7%,.2)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade",
+            "bg-color3 shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_hsla(206,22%,7%,.2)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade",
           )}
         >
           {props.fields.map((fieldsets, index) => (
@@ -78,7 +78,7 @@ const PopoverDemo = ({ ...props }: PopoverProps) => {
                     <input
                       className={classnames(
                         theme,
-                        "w-full inline-flex items-center justify-center flex-1 rounded leading-none text-color11 shadow-[0_0_0_1px] shadow-color7 focus:shadow-[0_0_0_2px] focus:shadow-color8 outline-none",
+                        "w-full inline-flex items-center justify-center flex-1 leading-none text-color11 shadow-[0_0_0_1px] shadow-color7 focus:shadow-[0_0_0_2px] focus:shadow-color8 outline-none",
                       )}
                       id={fieldset.id}
                       defaultValue={fieldset.defaultValue}
